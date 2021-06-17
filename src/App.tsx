2d437fdab3c8e47505d1dd8c1fdf2afe938f8c9f
Max Dayton <daytonmaximus@gmail.com>
2021-06-17 01:12:01 -0600

    faucet and get balance

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import './App.css'
+import FCTToken from './artifacts/contracts/FCTToken.sol/FCTToken.json'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import Faucet from './components/Faucet'
+
 const App = () => {
+    const Token = FCTToken 
+
     return (
-        <div>
-            <h1>Faucet</h1>
+        <div className="App">
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col>
+                        <Faucet tokenContract={Token} />
+                    </Col>
+                    <Col>
+                        <div> our send area</div>
+                    </Col>
+                </Row>
+            </Container>
+            
         </div>
     )
 };
